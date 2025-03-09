b76d5f99a08313cb114a497f7b9d98da8797b9d2
Demetrice Tuttle <dpt@MacBookPro.attlocal.net>
2025-03-09 14:54:08 -0400

    Changes

--- app.py
+++ app.py
@@ -231,5 +231,4 @@ def build_csv(results):
     writer.writerows(results)
     return output.getvalue()
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+#-------if __name__ == "__main__" app.run(debug=True, port=5000)
