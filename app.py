0c00117a6c067b7f44988a7a0e3aa7ca28e78301
Demetrice Paulo-Tuttle <107776117+dpTuttle@users.noreply.github.com>
2025-03-09 23:49:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -349,5 +349,4 @@ def combined_index():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+#------if __name__ == "__main__":    app.run(debug=True, port=5000)
